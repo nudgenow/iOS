b189babdeca7293441eafff3aa1bbf884493bbd5
GitHub Action <action@github.com>
2025-02-26 17:48:49 +0000

    iOS Staging release version 4.0.3 - Updated checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: packageName,
             url: "https://github.com/nudgenow/iOS/raw/refs/heads/prod_main/Nudgecore_iOS.xcframework.zip",
-            checksum: "1f49d146d26dddc673568906fe6c4d38c074d0a42fe99ef38ce395419efab5ed"
+            checksum: "8f3f625f0ec9a9e6561a7f64f333b1ae1b69b6ad7b8a3cd65b394795dcc9d0d2"
         ),
     ]
 )
