a9468d4ec315342cb37f9b1e1878c2259d998052
rndmcodeguy <shantanu.mane.200@outlook.com>
2025-04-03 16:10:33 +0530

    chore: removed deprecated library and updated Package version

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let packageName = "nudgecore_v2"
+let packageName = "Nudgecore_iOS"
 let package = Package(
-    name: "nudgecore_v2",
+    name: "Nudgecore_iOS",
     platforms: [.iOS(.v13)],
     products: [
         .library(name: packageName, targets: [packageName]),
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: "https://github.com/nudgenow/iOS/raw/refs/heads/prod_main/nudgecore_v2.xcframework.zip",
+            url: "https://github.com/nudgenow/iOS/raw/refs/tags/5.0.0/Nudgecore_iOS.xcframework.zip",
             checksum: "b0893eb349713bc9083722433f36291b0f0a038ec8c65dfcb5d3f0f5e8b6cc68"
         ),
     ]
