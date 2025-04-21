182360e4876ef565d8578c6b3ea7a3b2fbef8a71
GitHub Action <action@github.com>
2025-04-21 08:10:27 +0000

    iOS prod release version 5.0.8 - Updated checksum

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: "https://github.com/nudgenow/iOS/raw/refs/tags/5.0.7/Nudgecore_iOS.xcframework.zip",
-            checksum: "943f76e197a321cf82319bcce4ed256ddbed86288f61e4fa2f96064d6be01fee"
+            url: "https://github.com/nudgenow/iOS/raw/refs/tags/5.0.8/Nudgecore_iOS.xcframework.zip",
+            checksum: "fd43c9de74ae20bd17ada471761caa11c834c5366d9fb9be640a3de21633f58b"
         ),
     ]
 )
