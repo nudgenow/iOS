b3c5e329a52cbc8c28f3fad2d3bdfe43ee62f552
GitHub Action <action@github.com>
2025-05-09 12:21:47 +0000

    iOS prod release version 7.0.3 - Updated checksum

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: "https://github.com/nudgenow/iOS/raw/refs/tags/7.0.2/Nudgecore_iOS.xcframework.zip",
-            checksum: "6b315ddd71129095cfac1eab6660dbbfb641a111df686cb915902aa8cd48bd19"
+            url: "https://github.com/nudgenow/iOS/raw/refs/tags/7.0.3/Nudgecore_iOS.xcframework.zip",
+            checksum: "4dcbedccfadb77e2501136050f169e9c7cc49e61cc85d05df7e3c7c17485e44c"
         ),
         .binaryTarget(
             name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/6.8.1/RiveRuntime.xcframework.zip",
-            checksum: "100492b01fee6542952074f4f0107b003a67e1970f7d6c5686db6300d4c5629a"
+            url: "https://github.com/nudgenow/iOS/raw/refs/tags/7.0.3/Nudgecore_iOS.xcframework.zip",
+            checksum: "4dcbedccfadb77e2501136050f169e9c7cc49e61cc85d05df7e3c7c17485e44c"
         )
     ]
 )
