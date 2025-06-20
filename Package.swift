d90eb2c6500a20d6e2e823131e34d300d1202c0a
Jatin Menghani <jatin@nudgenow.com>
2025-06-20 12:25:30 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,18 +6,13 @@ let package = Package(
     name: "Nudgecore_iOS",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: packageName, targets: [packageName, "RiveRuntime"]),
+        .library(name: packageName, targets: [packageName])
     ],
     targets: [
         .binaryTarget(
             name: packageName,
             url: "https://github.com/nudgenow/iOS/raw/refs/tags/7.0.20/Nudgecore_iOS.xcframework.zip",
             checksum: "bb0d97e8f8061521164ae5ef185a6f116ade5c4950e830a3f1bfbf46f4365296"
-        ),
-        .binaryTarget(
-            name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/6.8.1/RiveRuntime.xcframework.zip",
-            checksum: "100492b01fee6542952074f4f0107b003a67e1970f7d6c5686db6300d4c5629a"
         )
     ]
 )
