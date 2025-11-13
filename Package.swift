07d629aaf9719d8e2f00364c55e311c6b78b035d
Jatin Menghani <jatin@nudgenow.com>
2025-11-13 18:27:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,20 +2,28 @@
 import PackageDescription
 
 let packageName = "Nudgecore_iOS"
+
 let package = Package(
-    name: "Nudgecore_iOS",
+    name: packageName,
     platforms: [.iOS(.v13)],
     products: [
         .library(name: packageName, targets: [packageName])
     ],
     dependencies: [
-        .package(url: "https://github.com/microsoft/plcrashreporter.git", exact: "1.12.0")
+        .package(url: "https://github.com/microsoft/plcrashreporter.git", from: "1.12.0")
     ],
     targets: [
         .binaryTarget(
             name: packageName,
             url: "https://github.com/nudgenow/iOS/raw/refs/tags/9.0.20/Nudgecore_iOS.xcframework.zip",
             checksum: "217929b7b6bd746b9e043fbba3b31c751a3b0e8c059836727f271b708f645e54"
+        ),
+        .target(
+            name: "\(packageName)_Wrapper",
+            dependencies: [
+                .byName(name: packageName),
+                .product(name: "CrashReporter", package: "plcrashreporter")
+            ]
         )
     ]
 )
